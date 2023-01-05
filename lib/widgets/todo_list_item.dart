d5dae7b8287aaabf77c380e985c2e746a026dafa
Jeova Henrique <jeovahenrique92@gmail.com>
2023-01-05 20:40:42 -0300

    animação deletar

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_list/models/todo.dart';
-
 
 class TodoListItem extends StatelessWidget {
   const TodoListItem({Key? key, required this.todo}) : super(key: key);
@@ -10,30 +10,44 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.grey[200],
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 5),
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Slidable(
+      endActionPane:  ActionPane(
+        motion: const DrawerMotion(),
+        extentRatio: 0.25,
         children: [
-          Text(
-            DateFormat("dd/MM/yyyy - HH:mm").format(todo.dateTime),
-            style: const TextStyle(
-              fontSize: 12,
-            ),
-          ),
-          Text(
-            todo.title,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            ),
+          SlidableAction(
+            label: "Delete",
+            icon: Icons.delete,
+            backgroundColor: Colors.red,
+            onPressed: (context) {},
           ),
         ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.grey[200],
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 5),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat("dd/MM/yyyy - HH:mm").format(todo.dateTime),
+              style: const TextStyle(
+                fontSize: 12,
+              ),
+            ),
+            Text(
+              todo.title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
